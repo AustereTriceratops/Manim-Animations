6a4271b285f202daaabc17eea33fd5cb071bd843
AustereTriceratops <39139710+AustereTriceratops@users.noreply.github.com>
2019-12-10 12:45:20 -0600

    Update Animation.py

--- HarmonicSums/Animation.py
+++ HarmonicSums/Animation.py
@@ -12,23 +12,6 @@ def equality(lhs, rhs):
     return VGroup(lhs_, eq_, rhs_)
 
 
-def function_curve(func, interval=(1,7), resolution=20):
-    width = interval[1] - interval[0]
-    step = width/resolution
-    x = [interval[0]]
-    y = [func(interval[0])]
-    curve_ = VGroup()
-
-    for i in range(1, resolution+1):
-        x.append(x[i-1] + step)
-        y.append(func(x[i]))
-        curve_.add(
-            Line(start=x[i-1]*RIGHT + y[i-1]*UP, end=x[i]*RIGHT + y[i]*UP)
-        )
-
-    return curve_
-
-
 class Main(Scene):
     def construct(self):
         eq1 = equality(r'\displaystyle\sum_1^{\infty}', r'1 + \frac{1}{2} + \frac{1}{3}+\frac{1}{4}+\cdots')
@@ -36,8 +19,7 @@ class Main(Scene):
 
         pluses = VGroup(*[TexMobject('+') for _ in range(1, 6)])
         bars = VGroup(*[Rectangle(height=2/n, width=1, color=BLACK) for n in range(1,7)])
-        #curve = function_curve(lambda x: 1/x).stretch(2, 1)
-        curve = ParametricFunction(function=lambda x: [x, 1/x, 0], t_min=1, t_max=7, step_size=0.2).stretch(2,1)
+        curve = ParametricFunction(function=lambda x: [x, 2/x, 0], t_min=1, t_max=7, step_size=0.2)
 
         bars_aligned = bars.copy()
         for i in range(0,6):
@@ -56,10 +38,6 @@ class Main(Scene):
 
         eq2 = VGroup(eq1[0].copy(), eq1[1].copy(), rhs_bars).center()
         eq3 = VGroup(eq1[0].copy(), eq1[1].copy(), bars_aligned, curve).center()
-
-        # it would be useful to have VGroups in which not all objects are rendered during transformations
-        # i.e. VGroups which serve only as containers for aligning/organizing mobjects
-        # this may be their intended use and I need to figure out how to properly use Transformations
 
         self.add(eq1)
         self.wait()
